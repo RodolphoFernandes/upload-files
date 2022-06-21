559c4c32f5f90ef6dd2ef2bf32248cd3af0bc511
Rodolpho Fernandes <rfernandes@epharma.com.br>
2022-06-21 09:11:15 -0300

    Finalizado o desafio

---
+++ src/components/CardList.tsx
@@ -0,0 +1,47 @@
+import { SimpleGrid, useDisclosure } from '@chakra-ui/react';
+import { useState } from 'react';
+import { Card } from './Card';
+import { ModalViewImage } from './Modal/ViewImage';
+
+interface Card {
+  title: string;
+  description: string;
+  url: string;
+  ts: number;
+  id: string;
+}
+
+interface CardsProps {
+  cards: Card[];
+}
+
+export function CardList({ cards }: CardsProps): JSX.Element {
+  const [currentImageUrl, setCurrentImageUrl] = useState('');
+  const { onOpen, isOpen, onClose } = useDisclosure();
+
+  function handleViewImage(url: string): void {
+    onOpen();
+    setCurrentImageUrl(url);
+  }
+  // TODO MODAL USEDISCLOSURE
+
+  // TODO SELECTED IMAGE URL STATE
+
+  // TODO FUNCTION HANDLE VIEW IMAGE
+
+  return (
+    <>
+      <SimpleGrid columns={[1, 2, 3]} spacing="40px">
+        {cards.map(card => (
+          <Card key={card.id} data={card} viewImage={handleViewImage} />
+        ))}
+      </SimpleGrid>
+
+      <ModalViewImage
+        isOpen={isOpen}
+        imgUrl={currentImageUrl}
+        onClose={onClose}
+      />
+    </>
+  );
+}
---
+++ src/components/Form/FormAddImage.tsx
@@ -0,0 +1,155 @@
+import { Box, Button, Stack, useToast } from '@chakra-ui/react';
+import { useForm } from 'react-hook-form';
+import { useState } from 'react';
+import { useMutation, useQueryClient } from 'react-query';
+
+import { api } from '../../services/api';
+import { FileInput } from '../Input/FileInput';
+import { TextInput } from '../Input/TextInput';
+
+interface FormAddImageProps {
+  closeModal: () => void;
+}
+
+interface ImageData {
+  url: string;
+  title: string;
+  description: string;
+}
+
+export function FormAddImage({ closeModal }: FormAddImageProps): JSX.Element {
+  const [imageUrl, setImageUrl] = useState('');
+  const [localImageUrl, setLocalImageUrl] = useState('');
+  const toast = useToast();
+
+  const formatsRegex =
+    /(?:([^:/?#]+):)?(?:([^/?#]*))?([^?#](?:jpeg|gif|png))(?:\?([^#]*))?(?:#(.*))?/g;
+
+  const formValidations = {
+    image: {
+      required: true,
+      validate: {
+        lessThan10MB: (file: File[]) =>
+          file?.[0].size < 10000000 || 'O arquivo deve ser menor que 10MB',
+        acceptedFormats: (file: File[]) =>
+          formatsRegex.test(file?.[0].type) ||
+          'Somente são aceitos arquivos PNG, JPEG e GIF',
+      }
+    },
+    title: {
+      required: 'Título obrigatório',
+      minLength: {
+        value: 2,
+        message: 'Mínimo de 2 caracteres',
+      },
+      maxLength: {
+        value: 20,
+        message: 'Máximo de 20 caracteres',
+      },
+    },
+    description: {
+      required: 'Descrição obrigatória',
+      maxLength: {
+        value: 65,
+        message: 'Máximo de 65 caracteres',
+      },
+    },
+  };
+
+  const queryClient = useQueryClient();
+  const mutation = useMutation(
+    async (image: ImageData) => {
+      await api.post('/api/images', {
+        ...image,
+        url: imageUrl,
+      });
+    },
+    // TODO MUTATION API POST REQUEST,
+    {
+      onSuccess: () => {
+        queryClient.invalidateQueries('images');
+      },
+      // TODO ONSUCCESS MUTATION
+    }
+  );
+
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState,
+    setError,
+    trigger,
+  } = useForm();
+  const { errors } = formState;
+
+  const onSubmit = async (data: ImageData): Promise<void> => {
+    try {
+      if (!imageUrl) {
+        toast({
+          status: 'error',
+          title: 'Imagem não adicionada',
+          description:
+            'É preciso adicionar e aguardar o upload de uma imagem antes de realizar o cadastro.',
+        });
+        return;
+      }
+      await mutation.mutateAsync(data);
+      toast({
+        title: 'Imagem cadastrada',
+        description: 'Sua imagem foi cadastrada com sucesso.',
+        status: 'success',
+      });
+    } catch {
+      toast({
+        title: 'Falha no cadastro',
+        description: 'Ocorreu um erro ao tentar cadastrar a sua imagem.',
+        status: 'error',
+      });
+    } finally {
+      reset();
+      setImageUrl('');
+      setLocalImageUrl('');
+      closeModal();
+    }
+  };
+
+  return (
+    <Box as="form" width="100%" onSubmit={handleSubmit(onSubmit)}>
+      <Stack spacing={4}>
+        <FileInput
+          setImageUrl={setImageUrl}
+          localImageUrl={localImageUrl}
+          setLocalImageUrl={setLocalImageUrl}
+          setError={setError}
+          trigger={trigger}
+          error={errors.image}
+          {...register('image', formValidations.image)}
+        />
+
+        <TextInput
+          placeholder="Título da imagem..."
+          error={errors.title}
+          {...register('title', formValidations.title)}
+        />
+
+        <TextInput
+          placeholder="Descrição da imagem..."
+          error={errors.description}
+          {...register('description', formValidations.description)}
+        />
+      </Stack>
+
+      <Button
+        my={6}
+        isLoading={formState.isSubmitting}
+        isDisabled={formState.isSubmitting}
+        type="submit"
+        w="100%"
+        py={6}
+      >
+        Enviar
+      </Button>
+    </Box>
+  );
+}
